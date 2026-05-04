ec082a9b79a95fbf4e5f1c19ee51eeafd5b5f743
nivanda <partlas.martin@gmail.com>
2026-05-04 20:35:21 +0300

    Added config class that detects if database.db file existed prior toJPA initialization

---
+++ src/main/java/net/nivanda/moviesapi/db_init/DBInitConfig.java
@@ -0,0 +1,23 @@
+package net.nivanda.moviesapi.db_init;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class DBInitConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Bean
+    public boolean dbFreshlyCreated() {
+        String path = dataSourceUrl.replace("jdbc:sqlite:", "");
+        File dbFile = new File(path);
+
+        boolean existed = dbFile.exists();
+        return !existed;
+    }
+}
