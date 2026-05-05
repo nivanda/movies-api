15d7519a11c3b777b37e0ba37e45dbe2e98272fc
nivanda <partlas.martin@gmail.com>
2026-05-05 16:15:50 +0300

    Added Actor Entity

--- src/main/java/net/nivanda/moviesapi/Entities/Actor.java
+++ src/main/java/net/nivanda/moviesapi/Entities/Actor.java
@@ -3,6 +3,8 @@ package net.nivanda.moviesapi.Entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,14 +16,43 @@ public class Actor {
 
     private String name;
 
+    private LocalDate birthDate;
+
     @ManyToMany
     @JoinTable(
-            name = "actor-movies",
+            name = "actors_movies",
             joinColumns = @JoinColumn(name = "actor_id"),
-            inverseJoinColumns = @JoinColumn(name = "movie-id"))
-    private Set<Movie> movies;
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private Set<Movie> movies = new HashSet<>();
 
-    public Actor(String name) {
+    public Actor() {}
+
+    public Actor(String name, LocalDate birthDate) {
         this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Set<Movie> getMovies() {
+        return this.movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
     }
 }
