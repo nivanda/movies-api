425a65613700e01dff04c3a5773ccdbb446f6d9e
nivanda <partlas.martin@gmail.com>
2026-05-11 22:03:14 +0300

    My brain hurts

--- src/main/java/net/nivanda/moviesapi/db_init/DBInitConfig.java
+++ src/main/java/net/nivanda/moviesapi/db_init/DBInitConfig.java
@@ -1,5 +1,6 @@
 package net.nivanda.moviesapi.db_init;
 
+import net.nivanda.moviesapi.Repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +14,11 @@ public class DBInitConfig {
     private String dataSourceUrl;
 
     @Bean
-    public boolean dbFreshlyCreated() {
+    public boolean dbFreshlyCreated(MovieRepository movieRepository) {
         String path = dataSourceUrl.replace("jdbc:sqlite:", "");
         File dbFile = new File(path);
 
-        boolean existed = dbFile.exists();
-        return !existed;
+        boolean existed = !dbFile.exists() || movieRepository.count() == 0;
+        return existed;
     }
 }
