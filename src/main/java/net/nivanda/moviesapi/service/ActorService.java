aac317750ad3df831ea01356fc2038b5fdbd64ef
nivanda <partlas.martin@gmail.com>
2026-05-19 11:20:33 +0300

    Added ActorService

---
+++ src/main/java/net/nivanda/moviesapi/service/ActorService.java
@@ -0,0 +1,28 @@
+package net.nivanda.moviesapi.service;
+
+import net.nivanda.moviesapi.entity.Actor;
+import net.nivanda.moviesapi.entity.Movie;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ActorService {
+
+    List<Actor> getAll();
+
+    Actor findById(Long id);
+
+    List<Actor> filterByName(String name);
+
+    Actor create(Actor actor);
+
+    Actor patchName(Long id, String name);
+
+    Actor patchBirthDate(Long id, LocalDate birthDate);
+
+    Actor patchMovies(Long actorId, List<Long> movieIDs);
+
+    List<Movie> getMoviesByActorId(Long id);
+
+    void delete(Long id);
+}
