779343f4bb865380c0f393e3a926e51dc503402d
nivanda <partlas.martin@gmail.com>
2026-05-28 00:26:35 +0300

    Finishing writing ActorController class

--- src/main/java/net/nivanda/moviesapi/controller/ActorController.java
+++ src/main/java/net/nivanda/moviesapi/controller/ActorController.java
@@ -2,19 +2,31 @@ package net.nivanda.moviesapi.controller;
 
 
 import net.nivanda.moviesapi.entity.Actor;
+import net.nivanda.moviesapi.entity.Movie;
+import net.nivanda.moviesapi.patch_dto.ActorPatchDTO;
 import net.nivanda.moviesapi.service.ActorService;
+import net.nivanda.moviesapi.service.MovieService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/actor")
+@RequestMapping("/api/actors")
 public class ActorController {
 
     private final ActorService service;
+    private final MovieService movieService;
 
-    public ActorController(ActorService service) {
+    public ActorController(ActorService service, MovieService movieService) {
         this.service = service;
+        this.movieService = movieService;
+    }
+
+    @GetMapping("?name={name}")
+    public List<Actor> filterByName(@PathVariable String name) {
+        return service.filterByName(name);
     }
 
     @PostMapping
@@ -30,5 +42,25 @@ public class ActorController {
     @GetMapping("/{id}")
     public Actor getOne(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Actor patch(@PathVariable Long id, @RequestBody ActorPatchDTO dto) {
+        Actor actor = service.findById(id);
+
+        if (dto.getName() != null) actor.setName(dto.getName());
+        if (dto.getBirthDate() != null) actor.setBirthDate(dto.getBirthDate());
+
+        if (dto.getMovieIDs() != null) {
+            Set<Movie> movies = dto.getMovieIDs().stream().map(movieService::findById).collect(Collectors.toSet());
+            actor.setMovies(movies);
+        }
+
+        return service.update(id, actor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 }
